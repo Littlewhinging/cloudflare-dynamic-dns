7b3393108dc73f9c76bf9c32d18d4596535426ab
Littlewhinging <owen.silman05@outlook.com>
2024-05-01 00:29:32 +0100

    done

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup
 
 setup(
-    name='cloudflare-dynamic-dns',
-    version='0.1.0',
+    name='cloudflare-dynamic-dns-client',
+		description='Allows selfhosting on cloudflare with a dynamic ip address',
+    version='0.1.2',
     packages=['cloudflare_dynamic_dns'],
+		author="Littlewhinging",
     install_requires=[
       "cloudflare==2.11.1",
 			"Requests==2.31.0"
