7e4376465888fe899061d3a16afbf17b3137a84b
Littlewhinging <owen.silman05@outlook.com>
2024-05-01 21:27:53 +0100

    New logging, fixed startup bug
    Co-authored-by: Will <TerraMonster5@users.noreply.github.com>

--- cloudflare_dynamic_dns/__main__.py
+++ cloudflare_dynamic_dns/__main__.py
@@ -1,60 +1,49 @@
 import os
-import CloudFlare
 import json
-from datetime import datetime
 import time
 import requests
+import logging
+import CloudFlare, CloudFlare.exceptions
 
-def intput(text):
-  textvalue = input(text)
-  try:
-    return int(textvalue)
-  except ValueError:
-    return 0
+dirpath = os.path.dirname(os.path.realpath(__file__))
 
-def menu(text,items):
-	for i in range(len(items)):
-		print(f"{i + 1}) {items[i]}")
-     
-	value = 0
-	while value < 1 or value > len(items):
-		value = intput(text)
-	print()
-	return value - 1
+FORMAT = "%(asctime)s:%(levelname)s:%(name)s:%(message)s"
+logging.basicConfig(filename=os.path.join(dirpath, "latest.log"), encoding="utf-8", level=logging.INFO, format=FORMAT)
+logger = logging.getLogger("main")
+strmHndlr = logging.StreamHandler()
+strmHndlr.setFormatter(logging.Formatter(FORMAT))
+logger.addHandler(strmHndlr)
 
+def intput(text: str) -> int:
+	textvalue = input(text)
+	try:
+		return int(textvalue)
+	except ValueError:
+		return 0
 
-def multi_menu(text,items):
+def multi_menu(text: str, items: list[str]) -> list[int]:
 	items.append("Finish")
 	exit_number = len(items)
 
-	for i in range(len(items)):
-		print(f"{i + 1}) {items[i]}")
- 
-	all_selected = []
+	for i, elem in enumerate(items, start=1):
+		print(f"{i}) {elem}")
+
+	all_selected: list[int] = []
 
 	while True:
 		value = 0
 		while value < 1 or value > len(items):
 			value = intput(text)
-   
+
 		if value == exit_number:
 			break
 
 		all_selected.append(value - 1)
-	
+
 	return all_selected
 
-def log(text):
-	time = datetime.now()
-	info = time.strftime("%Y-%m-%d %H:%M:%S") + "\t" + text
-	print(info)
-	with open("log.txt","a") as f:
-		f.write(info + "\n")
-
-def main():
-	use_config = os.path.exists("cfd-config.json")
-
-	if not use_config:
+def main() -> None:
+	if not os.path.exists(os.path.join(dirpath, "config.json")):
 		token = input("API Token: ")
 		try:
 			cf = CloudFlare.CloudFlare(token=token)
@@ -62,75 +51,83 @@ def main():
 		except:
 			print("Invalid API Token")
 			return
-		
+
 		dns_records = []
-  
+
 		for zone in zones:
 			try:
 				temp_dns_records = cf.zones.dns_records.get(zone["id"])
 			except CloudFlare.exceptions.CloudFlareAPIError as e:
-				exit('/zones/dns_records.get %d %s - api call failed' % (e, e))
+				logger.error("/zones/dns_records.get %d %s - api call failed" % (e, e))
+				exit("/zones/dns_records.get %d %s - api call failed" % (e, e))
 
 			temp_dns_records = [i for i in temp_dns_records if i["type"] == "A"]
-   
+
 			for i in range(len(temp_dns_records)):
 				temp_dns_records[i]["zone"] = zone["id"]
-    
+	
 			dns_records += temp_dns_records
-    
+
 		max_name_length = max([len(i["name"]) for i in dns_records])
 		selected = multi_menu("Select an option: ",[i["name"] + (" " * (max_name_length - len(i["name"]))) + "\t" + i["content"] for i in dns_records])
-		
+
 		selected_dns_records = [dns_records[i] for i in selected]
-  
+
 		zones = []
-  
+
 		for i in selected_dns_records:
 			if not i["zone"] in zones:
 				zones.append(i["zone"])
 
 		delay = intput("Delay between checks in seconds: ")
-    
-		with open("cfd-config.json","w") as f:
-			json.dump({"token": token, "zones": zones, "dns_records": [i["id"] for i in selected_dns_records], "delay": delay}, f)
+
+		selected_dns_records = [i["id"] for i in selected_dns_records]
+
+		with open(os.path.join(dirpath, "config.json"),"w") as f:
+			json.dump({"token": token, "zones": zones, "dns_records": selected_dns_records, "delay": delay}, f)
 
 	else:
-		with open("cfd-config.json","r") as f:
-			config = json.load(f)
-			token = config["token"]
-			zones = config["zones"]
-			selected_dns_records = config["dns_records"]
-			delay = config["delay"]
+		with open(os.path.join(dirpath, "config.json"),"r") as f:
+			CONFIG = json.load(f)
+			token = CONFIG["token"]
+			zones = CONFIG["zones"]
+			selected_dns_records = CONFIG["dns_records"]
+			delay = CONFIG["delay"]
 			cf = CloudFlare.CloudFlare(token=token)
 
 	if delay < 1:
 		delay = 1
 
-	log("")
-	log("[INFO] Program started")
+	logger.info("Program started")
 
-	ip = "Unknown"
+	ip = ""
+	first = True
 	while True:
 		new_ip = requests.get("https://4.ident.me").text
 
 		if ip != new_ip:
-			log(f"[INFO] IP changed from {ip} to {new_ip}")
+			if first:
+				logger.info(f"IP address is {new_ip}")
+				first = False
+			else:
+				logger.info(f"IP changed from {ip} to {new_ip}")
 			ip = new_ip
 
 			for zoneId in zones:
 				try:
 					temp_dns_records = cf.zones.dns_records.get(zoneId)
 				except CloudFlare.exceptions.CloudFlareAPIError as e:
-					log(f"[ERROR] Failed to fetch dns records for {zoneId}")
+					string = f"Failed to fetch dns records for {zoneId}\n" + "/zones/dns_records.get %d %s - api call failed" % (e, e)
+					logger.error(string)
+					continue
 				
 				for dns_record in temp_dns_records:
 					if dns_record["id"] in selected_dns_records and dns_record["content"] != ip:
 						dns_record["content"] = ip
 						cf.zones.dns_records.put(zoneId, dns_record["id"], data=dns_record)
-						log(f"[INFO] Updated record for {dns_record['name']}")
+						logger.info(f"Updated record for {dns_record['name']}")
 
 		time.sleep(delay)
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
 	main()
--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup
 
 setup(
-    name='cloudflare-dynamic-dns-client',
-		description='Allows selfhosting on cloudflare with a dynamic ip address',
-    version='0.1.2',
-    packages=['cloudflare_dynamic_dns'],
-		author="Littlewhinging",
-    install_requires=[
-      "cloudflare==2.11.1",
-			"Requests==2.31.0"
-    ],
-		entry_points={
-			'console_scripts': [
-				'cloudflare-dynamic-dns = cloudflare_dynamic_dns.__main__:main'
-			],
-    }
+	name='cloudflare-dynamic-dns-client',
+	description='Allows selfhosting on cloudflare with a dynamic ip address',
+	version='0.1.2',
+	packages=['cloudflare_dynamic_dns'],
+	author="Littlewhinging",
+	install_requires=[
+		"cloudflare==2.11.1",
+		"equests==2.31.0"
+	],
+	entry_points={
+		'console_scripts': [
+			'cloudflare-dynamic-dns = cloudflare_dynamic_dns.__main__:main'
+		],
+	}
 )
